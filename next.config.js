cad78b28697e7dd5788573430173b08d25b946e3
camiloJTG <camilojtg.devep@protonmail.com>
2023-06-27 20:47:11 -0400

    fix: Remove main page configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   async redirects() {
-      return [{ source: '/', destination: '/generator', permanent: true }];
-   }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
